8c7b4e84d80153cf0fa907f748c6121e61b8cb1a
Alejandro Menendez <alejandro.menendez.pull@gmail.com>
2018-04-25 17:17:11 +0530

    Initial Commit

---
+++ StringManipulation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StringManipulation
+//
+//  Created by Menlo on 25/04/18.
+//  Copyright © 2018 self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
