2bee300647e13ad5094ad7680fc2f0ea5dd8fc61
milankamilya <milankamilya@gmail.com>
2018-05-04 13:16:22 +0530

    ..

--- StringManipulationTests/StringManipulationTests.swift
+++ StringManipulationTests/StringManipulationTests.swift
@@ -158,7 +158,6 @@ class StringManipulationTests: XCTestCase {
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
-          
           // Put the code you want to measure the time of here.
         }
     }
